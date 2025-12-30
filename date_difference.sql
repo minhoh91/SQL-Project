bdb27bbe2f6f99320f462acddc8ab4d6befce28e
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-30 12:04:47 -0800

    Update date_difference.sql

--- date_difference.sql
+++ date_difference.sql
@@ -2,11 +2,12 @@
 
 SELECT
   u.user_id
-  , signup_date
+  , u.signup_date
   , MIN(transaction_date) as direct_deposit_date
-  , DATEDIFF(MIN(transaction_date, signup_date)
+  , DATEDIFF(MIN(transaction_date, signup_date) date_difference
 FROM users u
 JOIN transaction t
   ON u.user_id = t.user_id
 WHERE transaction_type = 'direct deposit'
-  ;
+GROUP BY u.user_id, u.signup_date
+;
