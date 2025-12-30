ba85d250cf1b89115f0ae064a42e737c28c9460b
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-30 09:47:35 -0800

    Update high_transactions.sql

--- high_transactions.sql
+++ high_transactions.sql
@@ -1,1 +1,11 @@
---Find a list of users who have exceeded 5 purchase transactions within a single calendar day, including the date and the specific count.
+-- Find a list of users who have exceeded 5 purchase transactions within a single calendar day, including the date and the specific count.
+SELECT
+  user_id
+  , transaction_date
+  , COUNT(transaction_id) AS transaction_count
+FROM transactions
+WHERE transaction_type = 'purchase'
+GROUP BY user_id, transaction_day
+HAVING COUNT(transaction_id) > 5
+ORDER BY COUNT(transaction_id) DESC;
+
