513e90e3cda12091a4862b1e3ac8cf8421883c15
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-30 12:02:52 -0800

    Update date_difference.sql

--- date_difference.sql
+++ date_difference.sql
@@ -1,1 +1,12 @@
 -- calculate the number of days between a user's signup_date and their very first direct_deposit
+
+SELECT
+  u.user_id
+  , signup_date
+  , MIN(transaction_date) as direct_deposit_date
+  , DATEDIFF(MIN(transaction_date, signup_date)
+FROM users u
+JOIN transaction t
+  ON u.user_id = t.user_id
+WHERE transaction_type = 'direct deposit'
+  ;
