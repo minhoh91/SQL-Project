75485248af4df0569efa4a449d6df80897284dc1
minhoh91 <108765289+minhoh91@users.noreply.github.com>
2025-12-31 10:41:07 -0800

    Update monthy_purchase.sql

--- monthy_purchase.sql
+++ monthy_purchase.sql
@@ -1,15 +1,23 @@
 -- Generate a list of users who were active (made a purchase) in one month AND also made a purchase in the following month.
 
-WITH transaction_history AS(
+WITH transactions_2 AS(
 SELECT
   DISTINCT user_id
-  , transaction_date
-  , LAG(transaction_date) OVER(PARTITION BY USER ID ORDER BY TRANSACTION DATE) previous_transaction_date
+  , EXTRACT(YEAR_MONTH FROM transaction_date) AS transaction_period
 FROM transactions
 WHERE transaction_type = 'purchase'
-)
+),
 
+WITH transactions_3 AS(
 SELECT
   user_id
-FROM transaction_history
-WHERE TIEMSTAMPDIFF(MONTH, previous_transaction_date, transaction_date) = 1;
+  , transaction_period
+  , LAG(transaction_period) OVER(PARTITION BY user_id ORDER BY transaction_period) AS previous_transaction_period
+FROM transactions_2
+)
+  
+SELECT
+  user_id
+FROM transactions_3
+WHERE PERIOD_DIFF(transaction_period, previous_transaction_period) = 1
+;
